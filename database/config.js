a90db8a26a0236f9419b5678e232a54a4e4820b9
Aryan Agarwal <aryankush025@gmail.com>
2022-08-26 19:03:14 +0530

    Quick fix in config.js

--- database/config.js
+++ database/config.js
@@ -1,21 +1,13 @@
 // For more sequelize commands see https://github.com/sequelize/cli#usage
 // & https://sequelize.org/v5/manual/migrations.html
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv/config');
 
-const dbConfig = {
+module.exports = {
   dialect: process.env.DATABASE_DIALECT,
   host: process.env.DATABASE_HOST,
   port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-};
-
-module.exports = {
-  development: dbConfig,
-  test: dbConfig,
-  production: dbConfig,
 };
